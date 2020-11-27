f5dd8e1cd3ca3fbfed40e6d5ac4242fb7c4eb201
Steve-dennis <74796745+Steve-dennis@users.noreply.github.com>
2020-11-27 15:32:26 -0600

    Update Animals.java
    updated to include zoo class

--- Animals.java
+++ Animals.java
@@ -1,13 +1,12 @@
 /**
- * Animals stores animals parent of StockRoom aggregation with Zoo
+ * Animals stores animals and enclosures parent of StockRoom 
  * @author StevenDennis AlexandraSuarez KylerYates AbdouToure 
  *
  */
 public class Animals
 {
     private String animal;
-    // aggregation with Zoo
-    Zoo animalEnclosure;
+    private String enclosure;
     /**
      * accessor method for animal
      * @return animal
@@ -23,11 +22,26 @@ public class Animals
         this.animal = animal;
     }
     /**
+     * accessor method for enclosure
+     * @return enclosure
+     */
+    public String getEnclosure() {
+		return enclosure;
+	}
+    /**
+     * mutator method for enclosure
+     * @param enclosure
+     */
+	public void setEnclosure(String enclosure) {
+		this.enclosure = enclosure;
+	}
+	/**
      * constructor with aggregation
      * @param animal
      */
-    public Animals( String animal, Zoo enclosure) {
-    	this.animalEnclosure = enclosure;
+    public Animals( String animal, String enclosure) {
+    	this.enclosure = enclosure;
         this.animal = animal;
     }
     
+}
