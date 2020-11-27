6994bec6e601b4d1da6aff106cda3d59ea02faaf
Steve-dennis <74796745+Steve-dennis@users.noreply.github.com>
2020-11-27 16:23:05 -0600

    Update Animals.java
    added  a way to count animals and the beginning of related animals by enclosure

--- Animals.java
+++ Animals.java
@@ -3,10 +3,13 @@
  * @author StevenDennis AlexandraSuarez KylerYates AbdouToure 
  *
  */
+import java.util.ArrayList;
 public class Animals
 {
     private String animal;
+    private int animalCount;
     private String enclosure;
+    
     /**
      * accessor method for animal
      * @return animal
@@ -36,12 +39,37 @@ public class Animals
 		this.enclosure = enclosure;
 	}
 	/**
+	 * accessor method for animalCount
+	 * @return animalCount
+	 */
+	public int getAnimalCount() {
+		return animalCount;
+	}
+	/**
+	 * mutator method for animalCount
+	 * @param animalCount
+	 */
+	public void setAnimalCount(int animalCount) {
+		this.animalCount = animalCount;
+	}
+	// method for showing related animals by enclosure
+	public ArrayList<String> animalGroup(){
+		ArrayList<String> group = new ArrayList<String>();
+		//TODO logic for grouping animals by enclosure and adding them to the group array
+		return (group);
+		
+	}
+
+	/**
      * constructor
      * @param animal
+     * @param enclosure
+     * @param animalCount
      */
-    public Animals( String animal, String enclosure) {
+    public Animals(String animal, String enclosure, int animalCount) {
     	this.enclosure = enclosure;
         this.animal = animal;
-    }
-    
+        this.animalCount = animalCount;
+
 }
+    }
