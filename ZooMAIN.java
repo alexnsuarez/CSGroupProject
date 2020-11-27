b76038bae123405ca9e862c5272022ed2e12c452
Alexandra Suarez <dreamleafalex@gmail.com>
2020-11-27 15:51:01 -0600

    Update ZooMAIN.java

--- ZooMAIN.java
+++ ZooMAIN.java
@@ -5,7 +5,56 @@
  */
 public class ZooMAIN {
 	public static void main(String [] args) {
-		// TODO create objects
+		Animals cheetah =
+			         new Animals("Cheetah", "Big Cats Enviornment");
+			Animals hippo =
+			         new Animals("Hippo", "African Beasts");
+			Animals lion =
+			         new Animals("Lion", "Big Cats Enviornment");
+			Animals tiger =
+					new Animals ("Tiger", "Big Cats Enviornment");
+			Animals rhino =
+					new Animals ("Rhino", "African Beasts");
+			Animals painteddog =
+					new Animals ("Painted Dog", "African Beasts");
+			Animals jackal =
+					new Animals ("Jackal", "African Beasts");
+			Animals crocodile =
+					new Animals ("Crocodile", "Reptile House");
+			Animals alligator =
+					new Animals ("Alligator", "Reptile House");
+			Animals turtle =
+					new Animals ("Turtle", "Reptile House");
+			Animals snake =
+					new Animals ("Snake", "Reptile House");
+			Animals blbear =
+					new Animals ("Black Bear", "Bear Territory");
+			Animals pbear =
+					new Animals ("Polar Bear", "Bear Territory");
+			Animals brbear =
+					new Animals ("Brown Bear", "Bear Territory");
+			Animals dolphin =
+					new Animals ("Dolphin", "Under the Sea");
+			Animals penguins =
+					new Animals ("Penguins", "Under the Sea");
+			Animals whales =
+					new Animals ("Whales", "Under the Sea");
+			Animals babboon =
+					new Animals ("Baboon", "Monkey Place");
+			Animals chimp =
+					new Animals ("Chimpanzee", "Monkey Place");
+			Animals gorilla =
+					new Animals ("Gorilla", "Monkey Place");
+			Animals flamingo =
+					new Animals ("Flamingo", "Birds of Paradise");
+			Animals parrot =
+					new Animals ("Parrot", "Birds of Paradise");
+			Animals poffin =
+					new Animals ("Poffin", "Birds of Paradise");
+		
+		
+		
+	
 		// TODO arraylist and loop can be used to create new objects
 		// TODO interface probably switch
 		
