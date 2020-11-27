ae2e851ff835c9f1704a50a9c26cb8dfc6b6dae1
Alexandra Suarez <dreamleafalex@gmail.com>
2020-11-27 16:56:40 -0600

    Update ZooMAIN.java

--- ZooMAIN.java
+++ ZooMAIN.java
@@ -1,70 +1,104 @@
 /**
- * ZooMAIN creates objects and interface
- * @author StevenDennis AlexandraSuarez KylerYates AbdouToure 
- *
- */
-public class ZooMAIN {
-	public static void main(String [] args) {
-		
-			Animals cheetah =
-			         new Animals("Cheetah", "Big Cats Enviornment");
-			Animals hippo =
-			         new Animals("Hippo", "African Beasts");
-			Animals lion =
-			         new Animals("Lion", "Big Cats Enviornment");
-			Animals tiger =
-					new Animals ("Tiger", "Big Cats Enviornment");
-			Animals rhino =
-					new Animals ("Rhino", "African Beasts");
-			Animals hyena =
-					new Animals ("Hyena", "African Beasts");
-			Animals jackal =
-					new Animals ("Jackal", "African Beasts");
-			Animals crocodile =
-					new Animals ("Crocodile", "Reptile House");
-			Animals alligator =
-					new Animals ("Alligator", "Reptile House");
-			Animals turtle =
-					new Animals ("Turtle", "Reptile House");
-			Animals snake =
-					new Animals ("Snake", "Reptile House");
-			Animals blbear =
-					new Animals ("Black Bear", "Bear Territory");
-			Animals pbear =
-					new Animals ("Polar Bear", "Bear Territory");
-			Animals brbear =
-					new Animals ("Brown Bear", "Bear Territory");
-			Animals dolphin =
-					new Animals ("Dolphin", "Under the Sea");
-			Animals penguins =
-					new Animals ("Penguin", "Under the Sea");
-			Animals whales =
-					new Animals ("Whales", "Under the Sea");
-			Animals babboon =
-					new Animals ("Baboon", "Monkey Place");
-			Animals chimp =
-					new Animals ("Chimpanzee", "Monkey Place");
-			Animals gorilla =
-					new Animals ("Gorilla", "Monkey Place");
-			Animals flamingo =
-					new Animals ("Flamingo", "Birds of Paradise");
-			Animals parrot =
-					new Animals ("Parrot", "Birds of Paradise");
-			Animals puffin =
-					new Animals ("Puffin", "Birds of Paradise");
+	 * ZooMAIN creates objects and interface
+	 * @author StevenDennis AlexandraSuarez KylerYates AbdouToure 
+	 *
+	 */
+	
+	public class ZooMAIN {
+		  
+		public static void main(String [] args) {
+			
 			
 			Scanner keyboard = new Scanner (System.in);
 			String searchedanimal;
-			searchedanimal = keyboard.nextLine();
+			String exhibit;
+			
 			
 			String[] listofanimals = {"Cheetah","Lion","Black Bear","Polar Bear", "Brown Bear", "Flamingo", "Parrot",
 					"Puffin", "Gorilla", "Chimpanzee", "Gorilla", "Babboon", "Whale", "Dolphin", "Penguin",
 					"Snake", "Turtle", "Alligator", "Crocodile", "Jackal", "Hyena", "Rhino", "Hippo", "Tiger"};
 			
-			boolean contains = Arrays.stream(listofanimals).anyMatch(searchedanimal::equals);
-		// TODO arraylist and loop can be used to create new objects
-		// TODO interface probably switch
+			System.out.println("Welcome to Carbondale's Zoo.");
+			System.out.println("Please enter an animal in the singualar form, capitalized.");
+			System.out.println("If nothing pops up, we may have the animal under a slightly more specific or general name, or we don't have that animal.");
+			System.out.println("You can search up to five animals before the program ends.");
+			searchedanimal = keyboard.nextLine();
+			
+			for (int count = 0; count < listofanimals.length; count++)
+			{
+			    if (searchedanimal.equals(listofanimals[count]))
+			    {
+			        System.out.println(listofanimals[count] + "s is one of the animals within the zoo.");
+			        
+			        if (searchedanimal.equals("Cheetah") || searchedanimal.equals("Lion") || searchedanimal.equals("Tiger"))
+			        		{
+			        		exhibit = "Big Cats Enviornment";
+			        		}
+			        
+			        else if (searchedanimal.equals("Hyena") || searchedanimal.equals("Jackal") || searchedanimal.equals("Hippo")|| searchedanimal.equals("Rhino"))
+	        				{
+			        		exhibit = "African Beasts Experience";
+			        	
+	        				}
+			        
+			        else if (searchedanimal.equals("Snake") || searchedanimal.equals("Turtle") || searchedanimal.equals("Crocodile")|| searchedanimal.equals("Alligator"))
+    						{
+			        		exhibit = "Reptile House";
+			        		
+    						}
+			        else if (searchedanimal.equals("Babboon") || searchedanimal.equals("Gorilla") || searchedanimal.equals("Chimpanzee"))
+							{
+			        		exhibit = "Monkey Place";
+			        	
+							}
+			        else if (searchedanimal.equals("Dolphin") || searchedanimal.equals("Whale") || searchedanimal.equals("Penguin"))
+							{
+			        		exhibit = "Under the Sea";
+			        	
+							}
+			        else if (searchedanimal.equals("Black Bear") || searchedanimal.equals("Polar Bear") || searchedanimal.equals("Brown Bear"))
+							{
+			        		exhibit = "Bear Territory";
+			   
+							}
+			        else if (searchedanimal.equals("Puffin") || searchedanimal.equals("Parrot") || searchedanimal.equals("Flamingo"))
+							{
+			        		exhibit = "Birds of Paradise";
+			        		
+							}
+			       
+			    }
+			 
+			    
+		Animals animalone =
+				new Animals (searchedanimal, exhibit);
 		
-	}
+		Animals animaltwo =
+				new Animals (searchedanimal, exhibit);
+		
+		Animals animalthree =
+				new Animals (searchedanimal, exhibit);
+		
+		Animals animalfour =
+				new Animals (searchedanimal, exhibit);
+		
+		Animals animalfive =
+				new Animals (searchedanimal, exhibit);
+			    	 
+			    
+			        }
+			 
+			}
+			
 
-}
+			
+
+			
+			
+			
+			
+			// TODO interface probably switch
+			
+		}
+
+	
